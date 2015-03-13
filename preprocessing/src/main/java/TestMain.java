a1ca0e003aaf01dd5944149fc9ac06573b0897f6
Benoit Seguin <seg.benoit@gmail.com>
2015-03-13 13:27:03 +0100

    Add of documentation, removal of lambda declarations.

--- preprocessing/src/main/java/TestMain.java
+++ preprocessing/src/main/java/TestMain.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.input.PortableDataStream;
 import org.apache.spark.mllib.linalg.Vector;
 import org.opencv.core.Core;
@@ -11,6 +12,7 @@ import org.opencv.core.Mat;
 
 public class TestMain {
 
+    //Load OpenCV JNI
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     public static void main(String[] args) {
@@ -18,11 +20,24 @@ public class TestMain {
         JavaSparkContext sc = new JavaSparkContext(conf);
         String inputFile = args[0];
         String outputFile = args[1];
+
+        //Get a handle for each file in the input directory
         JavaPairRDD<String,PortableDataStream> dataStream  = sc.binaryFiles(inputFile);
-        JavaPairRDD<String,ImageData> dataImages = dataStream.mapValues(new ImageDataCreator());
 
-        JavaPairRDD<String,Integer> pixelsNumber = dataImages.mapValues( (ImageData im) -> {Mat m =im.getImage(); return m.cols()*m.rows(); } );
+        //Convert the PortableDataStream to ImageData representations
+        JavaPairRDD<String,ImageData> dataImages = dataStream.mapValues( new Function<PortableDataStream, ImageData>() {
+            public ImageData call(PortableDataStream portableDataStream) throws Exception {
+                return new ImageData(portableDataStream.toArray());
+            } } );
 
+        //Get the number of pixels for each image
+        JavaPairRDD<String,Integer> pixelsNumber = dataImages.mapValues( new Function<ImageData, Integer>() {
+            public Integer call(ImageData im) {
+                    Mat m =im.getImage(); //Decompress and return a pointer to the uncompressed image representation
+                    return m.cols()*m.rows();
+            } } );
+
+        //Save result
         pixelsNumber.saveAsTextFile(outputFile);
         sc.close();
     }
