0764a348bc7d47b7be398dbf671ad6b0b53cf387
arvanito <nicklassmeister@gmail.com>
2015-03-26 17:12:10 +0100

    Double checked the correctness of the extraction.

--- preprocessing/src/main/java/ImageFunctions.java
+++ preprocessing/src/main/java/ImageFunctions.java
@@ -19,26 +19,33 @@ public class ImageFunctions {
      */
     static public List<Vector> extractPatches(Mat imgMat, int nbPatches) {
         List<Vector> results = new ArrayList<>();
-        int imgHorizontalSize = imgMat.cols();
-        int imgVerticalSize = imgMat.rows();
+
+        int numRows = imgMat.rows();
+        int numCols = imgMat.cols();
+        
+        System.out.println("Number of rows: " + numRows + " and number of columns: " + numCols);
+        
         //TODO get size from config file
-        int patchHorizontalSize = 96;
-        int patchVerticalSize = 96;
+        int patchRows = 32;
+        int patchCols = 32;
+        
         //extraction loop
         for(int n=0;n<nbPatches;n++) {
-            double[] patchData = new double[patchHorizontalSize*patchVerticalSize];
-            byte[] lineData = new byte[patchHorizontalSize];
+            double[] patchData = new double[patchRows*patchCols];
+            byte[] lineData = new byte[numCols];
             //find random left top corner
-            int iCol = (int)Math.round(Math.random()*(imgHorizontalSize-patchHorizontalSize));
-            int iRow = (int)Math.round(Math.random()*(imgVerticalSize-patchVerticalSize));
+            int iRow = (int)Math.round(Math.random()*(numRows-patchRows));
+            int iCol = (int)Math.round(Math.random()*(numCols-patchCols));
+            
             //extract from the corner line by line
-            for(int i=0;i<patchVerticalSize;i++) {
+            for(int i=0;i<patchRows;i++) {
                 //extract line from OpenCV
                 imgMat.get(iRow + i, iCol, lineData);
+                
                 //cast from byte to double
                 //TODO TO BE IMPROVED
-                for(int j=0;j<patchHorizontalSize;j++)
-                    patchData[i+patchVerticalSize*j]=(lineData[j]>=0? lineData[j] : 256+lineData[j]);
+                for(int j=0;j<patchCols;j++)
+                    patchData[i+patchRows*j]=(lineData[j]>=0? lineData[j] : 256+lineData[j]);
             }
             //
             results.add(new DenseVector(patchData));
