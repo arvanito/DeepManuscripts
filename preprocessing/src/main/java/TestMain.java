edd10b3f59785feb1465090446a68cd2e2aa45dc
Benoit Seguin <seg.benoit@gmail.com>
2015-03-26 13:27:37 +0100

    Separation of main class and load library class

--- preprocessing/src/main/java/TestMain.java
+++ preprocessing/src/main/java/TestMain.java
@@ -1,13 +1,11 @@
 package main.java;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkFiles;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.input.PortableDataStream;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import scala.Tuple2;
 
@@ -17,31 +15,9 @@ import java.util.List;
 
 public class TestMain {
 
-    static final String openCVLibName = System.mapLibraryName(Core.NATIVE_LIBRARY_NAME);
-    static final String openCVLibFullPath = "hdfs:///projects/deep-learning/lib/"+ openCVLibName;
-
-    /**
-     * Load OpenCV library, first try in local directory, if it does not work, switch to the file uploaded by the driver
-     */
-    static public void loadLibrary() {
-        try {
-            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-            System.out.println("OpenCV found in java.library.path");
-        }catch (UnsatisfiedLinkError e) {
-            System.out.println("OpenCV not found in java.library.path");
-            System.out.print("Trying SparkFiles...");
-            try {
-                System.load(SparkFiles.get(openCVLibName));
-                System.out.println("SUCCESS");
-            }catch (Exception e2) {
-                System.out.println("FAILURE");
-            }
-        }
-    }
-
-    //For worker nodes, will probably fail on driver node
+    //Ultimately a static declaration like that should do the job
     //static {
-    //    loadLibrary();
+    //    OpenCV.loadLibrary();
     //}
 
     public static void main(String[] args) {
@@ -57,7 +33,7 @@ public class TestMain {
             conf = new SparkConf().setAppName("DeepManuscript preprocessing");
             sc = new JavaSparkContext(conf);
             //Send OpenCV native lib to all worker
-            sc.addFile(openCVLibFullPath);
+            sc.addFile(OpenCV.openCVLibFullPath);
             inputFile = args[0];
             outputFile = args[1];
         }
@@ -66,11 +42,11 @@ public class TestMain {
         JavaPairRDD<String,PortableDataStream> dataStream  = sc.binaryFiles(inputFile);
 
         //Stupid (until I find better) way to load the library, apply an identity mapPartitions thingy that loads it
-        loadLibrary();
+        OpenCV.loadLibrary();
         dataStream = dataStream.mapPartitionsToPair(new PairFlatMapFunction<Iterator<Tuple2<String,PortableDataStream>>, String, PortableDataStream>() {
             @Override
             public Iterable<Tuple2<String, PortableDataStream>> call(Iterator<Tuple2<String, PortableDataStream>> input) throws Exception {
-                loadLibrary();
+                OpenCV.loadLibrary();
                 List<Tuple2<String, PortableDataStream> > l = new ArrayList<>();
                 while(input.hasNext())
                     l.add(input.next());
@@ -88,8 +64,9 @@ public class TestMain {
         JavaPairRDD<String, Integer> pixelsNumber = dataImages.mapValues(new Function<ImageData, Integer>() {
             public Integer call(ImageData im) {
                 Mat m = im.getImage(); //Decompress and return a pointer to the uncompressed image representation
-                    return m.cols()*m.rows();
-            } } );
+                return m.cols() * m.rows();
+            }
+        });
 
         //Save result
         pixelsNumber.saveAsTextFile(outputFile);
