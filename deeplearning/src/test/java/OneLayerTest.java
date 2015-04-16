6809966d5355883d07bc62040af20a34c0582267
Viviana Petrescu <petrescu.viviana@gmail.com>
2015-04-16 17:19:28 +0200

    BugFix: BaseLayer execute method parameters were confusing. Made OneBaseLayer unittest run

--- deeplearning/src/test/java/OneLayerTest.java
+++ deeplearning/src/test/java/OneLayerTest.java
@@ -34,7 +34,7 @@ public class OneLayerTest implements Serializable {
 
 	@Before
 	public void setUp() throws Exception {
-		sc = new JavaSparkContext("local", "MaxPoolerTest");
+		sc = new JavaSparkContext("local", "OneLayerTest");
 	}
 
 	@After
@@ -45,7 +45,6 @@ public class OneLayerTest implements Serializable {
 
 	@Test
 	public void test() throws Exception {
-//		fail("Not yet implemented");
 		ConfigBaseLayer.Builder conf = ConfigBaseLayer.newBuilder();
 		conf.setConfigPreprocess(ConfigPreprocess.newBuilder().setEps1(0.1).setEps2(0.2).build());
 		conf.setConfigFeatureExtractor(ConfigFeatureExtractor.newBuilder().
@@ -82,13 +81,51 @@ public class OneLayerTest implements Serializable {
 	
 		JavaRDD<Vector> represent = layer.extractFeatures(imgwords, c, features);
 		List<Vector> t = represent.collect();
+		Assert.assertEquals(50, t.size());
 		JavaRDD<Vector> pooled = layer.pool(represent);
 		List<Vector> result = pooled.collect();
 		Assert.assertEquals(50, result.size());
-	//	return pooled;
-		//JavaRDD<Vector> result = layer1.execute(data1, data1);
-			// How does copying of data happen here?
-			//input_small_patches = result
+		Assert.assertEquals(9, result.get(0).size());
+
+	}
+	
+	@Test
+	public void testBaseLayer() throws Exception {
+		ConfigBaseLayer.Builder conf = ConfigBaseLayer.newBuilder();
+		conf.setConfigPreprocess(ConfigPreprocess.newBuilder().setEps1(0.1).setEps2(0.2).build());
+		conf.setConfigFeatureExtractor(ConfigFeatureExtractor.newBuilder().
+						                  setFeatureDim1(2).setFeatureDim2(2).setInputDim1(8).setInputDim2(8).build());
+		conf.setConfigPooler(ConfigPooler.newBuilder().setPoolSize(2));
+	 	conf.setConfigKmeans(ConfigKMeans.newBuilder().setNumberOfClusters(3).setNumberOfIterations(10).build());	
+	 	ConfigBaseLayer c = conf.build();
+	 	
+	 	int Nimgs = 50;
+	 	int Npatches = 100;
+	 	List<Vector> input_word_patches = new ArrayList<Vector>(Nimgs);
+	 	double[] temp = new double[64];
+ 		for (int j = 0; j < 64; ++j) {
+ 			temp[j] = (double)j;
+ 		}
+	 	for (int i = 0; i < Nimgs; ++i) {
+	 		input_word_patches.add(Vectors.dense(temp));
+	 	}
+	 	
+	 	List<Vector> input_small_patches = new ArrayList<Vector>(Npatches);
+	 	for (int i = 0; i < Npatches; ++i) {
+	 		input_small_patches.add(Vectors.dense(1,2,3,4));
+	 	}
+	 	
+		DeepLearningLayer layer = BaseLayerFactory.createBaseLayer(c);
+		// We have 100 patches of size 2x2 as input
+		// We have 50 word images of size 8x8
+		JavaRDD<Vector> patches = sc.parallelize(input_small_patches);
+		JavaRDD<Vector> imgwords = sc.parallelize(input_word_patches);
+		
+		JavaRDD<Vector> result = layer.execute(patches, imgwords);
+
+		List<Vector> res = result.collect();
+		Assert.assertEquals(50, res.size());
+		Assert.assertEquals(9, res.get(0).size());
 
 	}
 }
