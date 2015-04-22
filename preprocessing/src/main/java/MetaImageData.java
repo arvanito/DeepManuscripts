3745a8f592c28d75fa501c9178a69e3cfbd8245c
Benoit Seguin <seg.benoit@gmail.com>
2015-04-22 13:59:02 +0200

    Corrections to the MetaData and MetaImageData classes. Removal/adds of some fields, cloneable interface for Metadata...

--- preprocessing/src/main/java/MetaImageData.java
+++ preprocessing/src/main/java/MetaImageData.java
@@ -21,93 +21,20 @@ public class MetaImageData implements Serializable {
 
     //Status of the representation
         
-    public ImageData Imgdata;
-    private Metadata metadata;
+    public ImageData imgdata;
+    public Metadata metadata;
 
-    //Probably other image metadata needed (compression type, compression level, precision of data, number of channels)
-    
     /**
      * Constructor Simple
      */
-    public MetaImageData(byte[] data,int x, int y, int w, int h) {
-    	this.Imgdata = new ImageData(data);
-    	this.metadata = new Metadata(x,y,w,h);
-    }
-    
-    
-    public void setX(int x) {
-        this.metadata.x_coord = x;
-    }
-    
-    public void setY(int x) {
-        this.metadata.y_coord = x;
-    }
-    
-    public void setWidth(int x) {
-        this.metadata.width = x;
-    }
-    
-    public void setHeight(int x) {
-        this.metadata.height = x;
-    }
-    
-    public int getX() {
-    	if(this.metadata.x_coord !=(Integer) null)
-    		return this.metadata.x_coord;
-    	else {
-    		System.out.println("X coordinate is NULL");
-    		return (Integer) null;
-    	}
-    }
-    
-    public int getY() {
-    	if(this.metadata.y_coord !=(Integer) null)
-    		return this.metadata.y_coord;
-    	else {
-    		System.out.println("Y coordinate is NULL");
-    		return (Integer) null;
-    	}
-    }
-    
-    public int getWidth() {
-    	if(this.metadata.width !=(Integer) null)
-    		return this.metadata.width;
-    	else {
-    		System.out.println("Width of the image is NULL");
-    		return (Integer) null;
-    	}
-    }
-    
-    public int getHeight() {
-    	if(this.metadata.height !=(Integer) null)
-    		return this.metadata.height;
-    	else {
-    		System.out.println("Height of the image is NULL");
-    		return (Integer) null;
-    	}
+    public MetaImageData(byte[] data) {
+    	this.imgdata = new ImageData(data);
+    	this.metadata = new Metadata();
     }
 
-    /**
-     * Serialization of Object
-     * @param out
-     * @throws IOException
-     */
-    public void writeObject(ObjectOutputStream out) throws IOException {
-        //write normal fields
-        out.defaultWriteObject();
-        
-    }
-
-    /**
-     * Serialization of Object
-     * @param in
-     * @throws IOException
-     * @throws ClassNotFoundException
-     */
-    public void readMetaObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        //read normal fields
-        in.defaultReadObject();
-        
+    public MetaImageData(byte[] data, Metadata _metadata) {
+        this.imgdata = new ImageData(data);
+        this.metadata = (Metadata)(_metadata.clone());
     }
 
 }
--- preprocessing/src/main/java/Metadata.java
+++ preprocessing/src/main/java/Metadata.java
@@ -2,31 +2,34 @@ package main.java;
 
 import java.io.Serializable;
 
-
-public class Metadata implements Serializable {
+/**
+ *
+ */
+public class Metadata implements Serializable, Cloneable {
     
     private static final long serialVersionUID = -7311690903847009641L;	
     public int x_coord,y_coord;
-    public int width, height;
+	public int page_number, line_number;
+	public String page_name;
     
     //More parameters can be added such as pageNo, lineNo, etc...
     
     // Null Constructor
     public Metadata() {
-    	x_coord = (Integer) null;
-    	y_coord = (Integer) null;
-    	width = (Integer) null;
-    	height = (Integer) null;
+    	x_coord = 0;
+    	y_coord = 0;
+		page_number = 0;
+		line_number = 0;
+		page_name = "";
     }
-    
-    //Simple Constructor
-    public Metadata(int x, int y, int w, int h) {
-    	x_coord = x;
-    	y_coord = y;
-    	width = w;
-    	height = h;
-    }
-    
-        
+
+	@Override
+	public Object clone() {
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			throw new Error("Something impossible just happened");
+		}
+	}
 
 }
