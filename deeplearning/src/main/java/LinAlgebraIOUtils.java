10926edf51b0d7e5051295374a290c768981dd2e
Viviana Petrescu <petrescu.viviana@gmail.com>
2015-04-25 19:29:07 +0200

    added LinAlgebraIOUtils

---
+++ deeplearning/src/main/java/LinAlgebraIOUtils.java
@@ -0,0 +1,56 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+
+/**
+ *  The class offers different ways of storing DenseMatrix and Vector from linalg package to hdfs.
+ *  The objects can be saved either as text file or as objects.
+ * 
+ * @author Viviana Petrescu
+ *
+ */
+
+public class LinAlgebraIOUtils {
+	public static void saveToText(Vector input, String outFile, JavaSparkContext sc) {
+		List<Vector> temp_input = new ArrayList<Vector>();
+		temp_input.add(input);
+		// Transform it to JavaRDD and save it to file
+		sc.parallelize(temp_input).saveAsTextFile(outFile);
+	}
+	public static void saveToObject(Vector input, String outFile, JavaSparkContext sc) {
+		List<Vector> temp_input = new ArrayList<Vector>();
+		temp_input.add(input);
+		// Transform it to JavaRDD and save it to file
+		sc.parallelize(temp_input).saveAsObjectFile(outFile);
+	}
+	public static Vector loadFromText(String inFile, JavaSparkContext sc) {
+		// Read back the file as an array of strings
+		JavaRDD<String> in_read = sc.textFile(inFile);
+
+		List<String> in_string = in_read.collect();
+		// Since it is a vector, it has 1 dimension equal to 1
+		assert(in_string.size() == 1);
+		System.out.println(in_string);
+		String m = in_string.get(0);
+		m = m.substring(1, m.length()-2);
+		String[] parts = m.split(",");
+		int vector_size = parts.length;
+		double out_vector[] = new double[vector_size];
+		for (int i = 0; i < vector_size; ++i) {
+			out_vector[i] = Double.parseDouble(parts[i]);
+		}
+		return Vectors.dense(out_vector);
+	}
+	public static Vector loadFromObject(String inFile, JavaSparkContext sc) {
+		JavaRDD<Vector> out = sc.objectFile("inFile");
+		List<Vector> a = out.collect();
+		return Vectors.dense(a.get(0).toArray());
+	}
+	
+}
--- deeplearning/src/test/java/LoadSaveModelTest.java
+++ deeplearning/src/test/java/LoadSaveModelTest.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.Assert;
+import main.java.LinAlgebraIOUtils;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.DenseVector;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.junit.After;
@@ -36,31 +38,28 @@ public class LoadSaveModelTest {
 	}
 
 	@Ignore @Test
-	public void testDenseVectorIO() {
+	public void testDenseVectorTextIO() {
+		// Create a sample mean vector 
+		Vector mean = Vectors.dense(1,2,3,4);
+		String filename = "tmp6";
+		LinAlgebraIOUtils.saveToText(mean, filename + "_mean", sc);
+		
+		// Read back the file as an array of strings
+		Vector reconstructed = LinAlgebraIOUtils.loadFromText(filename + "_mean", sc);
+		Assert.assertEquals(mean.toString(), reconstructed.toString());
+	}
+	@Ignore @Test
+	public void testDenseVectorObjectIO() {
 		// Create a sample mean vector 
 		Vector mean = Vectors.dense(1,2,3,4);
 		List<Vector> temp_mean = new ArrayList<Vector>();
 		temp_mean.add(mean);
-		String filename = "tmp3";
-		// Transform it to JavaRDD and save it to tmp1_mean file
-		sc.parallelize(temp_mean).saveAsTextFile(filename + "_mean");
+		sc.parallelize(temp_mean).saveAsObjectFile("t2");
 		
-		// Read back the file as an array of strings
-		JavaRDD<String> mean_read = sc.textFile(filename + "_mean");
-		
-		List<String> mean_string = mean_read.collect();
-		assert(mean_string.size() == 1);
-		System.out.println(mean_string);
-		String m = mean_string.get(0);
-		m = m.substring(1, m.length()-2);
-		String[] parts = m.split(",");
-		int vector_size = parts.length;
-		double mean_vector[] = new double[vector_size];
-		for (int i = 0; i < vector_size; ++i) {
-			mean_vector[i] = Double.parseDouble(parts[i]);
-		}
-		Vector reconstructed = Vectors.dense(mean_vector);
-		Assert.assertEquals(mean.toString(), reconstructed.toString());
+	    // Read back the array
+		JavaRDD<Vector> out = sc.objectFile("t2");
+		List<Vector> a = out.toArray();
+		Assert.assertEquals(mean.toString(),a.get(0).toString());
 	}
 
 }
