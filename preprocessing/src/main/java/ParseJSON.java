5b1b6fdf8475545d64afc93214fb7b79ca7ddb40
isinsukatircioglu <isinsu.katircioglu@gmail.com>
2015-04-27 13:55:59 -0700

    Added naive patch extraction

--- preprocessing/src/main/java/ParseJSON.java
+++ preprocessing/src/main/java/ParseJSON.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,26 +16,35 @@ import org.json.simple.parser.ParseException;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.DenseVector;
 import org.apache.spark.mllib.linalg.Vector;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.highgui.Highgui;
 import org.apache.spark.api.java.function.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.CvType;
 
 
 public class ParseJSON {
 
-	public int sizex = 64, sizey = 64;
-	
-	public void main (String[] args) throws IOException, ParseException {
+	public static int sizex = 64, sizey = 64;
+	public static Mat img; 
+	public static int numRows ;
+    public static int numCols ;
+    
+	public static void main (String[] args) throws IOException, ParseException {
 		
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-		Mat img = Highgui.imread("/home/isinsu/Desktop/DeepManuscripts/preprocessing/IS5337_2_010_00093.tif");
+		img = Highgui.imread("/home/isinsu/Desktop/DeepManuscripts/preprocessing/IS5337_2_010_00093.tif");
 		String filePath = "/home/isinsu/Desktop/DeepManuscripts/preprocessing/IS5337_2_010_00093.json";
+		Imgproc.cvtColor(img,img, Imgproc.COLOR_BGR2GRAY);
+		numRows = img.rows();
+	    numCols = img.cols();
 		processLines(img, filePath, "--local");
 	}
 	
-	public void processLines(Mat imgMat, String filePath, String arg)
+	public static void processLines(Mat imgMat, String filePath, String arg)
 	{
 		SparkConf conf;
 		JavaSparkContext sc;
@@ -47,7 +58,7 @@ public class ParseJSON {
 		}
 		
 		 ArrayList<Integer[][]> lines = parseJSON(filePath);
-		 System.out.println("Line number: "+ lines.size());
+		// System.out.println("Line number: "+ lines.size());
 		 JavaRDD<Integer[][]> textLines = sc.parallelize(lines);
 		 JavaRDD<List<Vector>> patches = textLines.map(new Function<Integer[][], List<Vector>> (){
 			@Override
@@ -57,23 +68,90 @@ public class ParseJSON {
 			}
 			
 		 });
-		 
-		 sc.close();
+		
+		patches.saveAsTextFile("/home/isinsu/Desktop/DeepManuscripts/preprocessing/output");
+		sc.close();
+//		ArrayList<Integer[][]> lines = parseJSON(filePath);
+//		System.out.println("Line number: "+ lines.size());
+//		for(int i = 0; i < lines.size(); i++)
+//		{
+//			List<Vector> res =  extractPatches(lines.get(i));
+//			System.out.println(res.size());
+//		}
+		
 	}
 	
-	public List<Vector> extractPatches(Integer[][] boundary) {
+	 public static void sortArray(Integer myArray[][], final int dim)
+	 { 
+	    Arrays.sort(myArray, new Comparator<Integer[]>()
+	    { 
+	            @Override 
+	            public int compare(Integer[] o1, Integer[] o2) 
+	            {
+	                    return o1[dim].compareTo(o2[dim]); 
+	            } 
+	    }); 
+	 }
+	
+	public static List<Vector> extractPatches(Integer[][] boundary) {
 
-	List<Vector> results = new ArrayList<>();
-	int stepSize;
-	if(sizex > sizey)
-		stepSize = sizey/2;
-	else
-		stepSize = sizex/2;
-
-	return results;
+		List<Vector> results = new ArrayList<>();
+		int stepSize;
+		if(sizex > sizey)
+			stepSize = sizey/2;
+		else
+			stepSize = sizex/2;
+		
+		sortArray(boundary,0);
+		int xmin = boundary[0][0];
+		int xmax = boundary[boundary.length-1][0];
+		sortArray(boundary,1);
+		int ymin = boundary[0][1];
+		int ymax = boundary[boundary.length-1][1];
+		//System.out.println(xmin + " " + xmax + " " + ymin + " " + ymax);
+		
+		for(int i = xmin; i <= xmax ; i = i+stepSize)
+		{
+			for(int j = ymin; j <= ymax ; j= j+stepSize)
+			{
+				double[] patchData = new double[sizex*sizey];
+				byte[] lineData = new byte[numCols];
+				Mat pat = new Mat();
+				if((i-stepSize+1 >= 0) && (i+stepSize+1 <= numRows) && (j-stepSize+1 >= 0) && (j+stepSize+1 <= numCols))
+				{
+					if(stepSize % 2 == 0)
+					{
+						pat = img.submat(i-stepSize+1, i+stepSize+1, j-stepSize+1, j+stepSize+1);
+						//System.out.println(pat.size().height);
+					}
+					else
+					{
+						pat = img.submat(i-stepSize, i+stepSize+1, j-stepSize, j+stepSize+1);
+					}
+					//Highgui.imwrite("/home/isinsu/Desktop/DeepManuscripts/preprocessing/pat.tif", pat);
+					
+					Mat temppatch = pat.clone();
+					Mat patch = temppatch.t();
+					patch = patch.reshape(0, 1);
+					//System.out.println(patch.rows() + " " + patch.cols());
+					for(int k = 0; k < patch.cols(); k++)
+					{
+						byte [] data = new byte[1];
+						patch.get(0, k, data);
+						patchData[k] = (data[0]>= 0 ? data[0] : 256+data[0]);
+					}
+	
+					results.add(new DenseVector(patchData)); 
+					
+				}
+				
+			}
+		}
+		
+		return results;
 	}
 
-	private ArrayList<Integer[][]> parseJSON(String filePath){
+	private static ArrayList<Integer[][]> parseJSON(String filePath){
 		
 		ArrayList<Integer[][]> textLines = new ArrayList<Integer[][]>();
 		
@@ -95,12 +173,12 @@ public class ParseJSON {
 			
 			//get the page_id			
 			String page_id = (String) page.get("id");
-			System.out.println("page_id  = " + page_id);
+			//System.out.println("page_id  = " + page_id);
 			
 			//get the page height and width
 			long height = (long) page.get("height");
 			long width = (long) page.get("width");
-			System.out.println("height = " + height + "  width = " + width);
+			//System.out.println("height = " + height + "  width = " + width);
 			
 			//get the number of lines
 			JSONArray lines = (JSONArray) page.get("lines");
@@ -115,7 +193,7 @@ public class ParseJSON {
 				
 				//get the line_id
 				long line_id = (long) line.get("id");
-				System.out.println("line_id =  " + line_id);
+				//System.out.println("line_id =  " + line_id);
 				
 				//get the x,y,w,h for the line
 				long x = (long) line.get("x");
@@ -169,7 +247,7 @@ public class ParseJSON {
 		
 	}
 
-	private  Integer[][] get_boundaries(String boundaries) {
+	private  static Integer[][] get_boundaries(String boundaries) {
 		// gets the two dimensional array of the form [size][2] where size is the number 
 		// of boundary points and 2 corresponds to the two indices x,y ..
 		// "boundaries" string is in the form ((a,b),(c,d),(e,f),.......,(y,z))
