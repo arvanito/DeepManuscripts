640c857a81b0251c6e5710fa3ef5406d028cda67
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2015-05-05 16:35:43 +0200

    Addition of Normal and Mutual graph

--- deeplearning/src/main/java/WeightedMatrix.java
+++ deeplearning/src/main/java/WeightedMatrix.java
@@ -11,8 +11,10 @@ public class WeightedMatrix {
 	private Vector[] input;
 	private Matrix output;
 	private double maxDistance;
-	private int type;
+	private int matrixType;
 	private int k;
+	private int neighborType;
+	private double sigma;
 
 	/**
 	 * @return the weighted Matrix
@@ -24,11 +26,52 @@ public class WeightedMatrix {
 		return output;
 	}
 
-	public WeightedMatrix(Vector[] input, int k, int type) {
+	/**
+	 * Constructor for WeightedMatrix
+	 * 
+	 * @param input
+	 *            Array of Vectors
+	 * @param k
+	 *            Number of nearest neighbors
+	 * @param matrixType
+	 *            The type of the result matrix:
+	 *            <ul>
+	 *            <li>0: for a matrix with only 1s and 0s.
+	 *            <li>1: for a weighted matrix
+	 *            </ul>
+	 * @param neighborType
+	 *            The type of the kNN matrix:
+	 *            <ul>
+	 *            <li>0: Not symmetric
+	 *            <li>1: Normal
+	 *            <li>2: Mutual
+	 *            </ul>
+	 */
+	public WeightedMatrix(Vector[] input, int k, int matrixType,
+			int neighborType, double sigma) {
 		this.input = input;
 		this.maxDistance = 0;
-		this.type = type;
+		this.matrixType = matrixType;
 		this.k = k;
+		this.neighborType = neighborType;
+		this.sigma = sigma;
+	}
+
+	/**
+	 * Default constructor for WeightedMatrix
+	 * 
+	 * @param input
+	 *            Array of Vectors
+	 * @param k
+	 *            Number of nearest neighbors
+	 */
+	public WeightedMatrix(Vector[] input, int k) {
+		this.input = input;
+		this.maxDistance = 0;
+		this.matrixType = 1;
+		this.k = k;
+		this.neighborType = 1;
+		this.sigma =1;
 	}
 
 	/**
@@ -37,46 +80,86 @@ public class WeightedMatrix {
 	public void compute() {
 		double[][] distances = computeDistanceMatrix();
 		double[][] kKeighbour = computeKNeighbours(distances);
+		if (neighborType == 1) {
+			kKeighbour = atMostK(kKeighbour);
+		} else if (neighborType == 2) {
+			kKeighbour = atLeastK(kKeighbour);
+		}
 		int n = input.length;
 		double[] result = new double[n * n];
-		if (type == 0) {
+		if (matrixType == 0) {
 			for (int i = 0; i < n; i++) {
-				for (int j = i; j < n; j++) {
+				for (int j = 0; j < n; j++) {
 					double w = 0;
-					if (distances[i][j] < maxDistance / 2) {
+					if (kKeighbour[i][j] > 0) {
 						w = 1;
 					}
 					result[i + j * n] = w;
-					result[j + i * n] = w;
 				}
 			}
 		} else {
 			for (int i = 0; i < n; i++) {
-				for (int j = i; j < n; j++) {
-					double w = (maxDistance - distances[i][j]) / maxDistance;
+				for (int j = 0; j < n; j++) {
+					double w = gaussian(kKeighbour[i][j]);
 					result[i + j * n] = w;
-					result[j + i * n] = w;
 				}
 			}
 		}
 		output = Matrices.dense(n, n, result);
 	}
 
+	private double gaussian(double d) {
+		return Math.exp(-Math.pow(d, 2)/(2*Math.pow(sigma, 2)));
+	}
+
+	private double[][] atLeastK(double[][] kNeighbour) {
+		double[][] result = kNeighbour;
+		int size = result.length;
+		for (int i = 0; i < size; i++) {
+			for (int j = i; j < size; j++) {
+				double a = Math.max(result[i][j], result[j][i]);
+				result[i][j] = a;
+				result[j][i] = a;
+			}
+		}
+		return result;
+	}
+
+	private double[][] atMostK(double[][] kNeighbour) {
+		double[][] result = kNeighbour;
+		int size = result.length;
+		for (int i = 0; i < size; i++) {
+			for (int j = i; j < size; j++) {
+				double a = Math.min(result[i][j], result[j][i]);
+				result[i][j] = a;
+				result[j][i] = a;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Compute a non-symmetric matrix by keeping for each row the distance of
+	 * the <i>k</i> nearest points, and setting the rest to 0.
+	 * 
+	 * @param distances
+	 * @return
+	 */
 	private double[][] computeKNeighbours(double[][] distances) {
 		int size = distances.length;
-		double[][] results = invertMatrix(distances);
+		double[][] results = distances;
 		if (k >= size - 1) {
 			return results;
 		} else {
 			for (int i = 0; i < size; i++) {
 				double[][] toSort = new double[size][2];
 				for (int j = 0; j < size; j++) {
-					toSort[j][2] = j;
-					toSort[j][1] = results[i][j];
+					toSort[j][1] = j;
+					toSort[j][0] = results[i][j];
 				}
 				double[][] sorted = sortWeights(toSort);
-				for (int j = 0; j < size - k - 1; j++) {
-					results[i][(int) sorted[j][2]] = 0;
+				for (int j = k + 1; j < size; j++) {
+					results[i][(int) sorted[j][1]] = 0;
 				}
 			}
 			return results;
