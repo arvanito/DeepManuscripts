6a3fa50c3e1156500340675b5dfb17743c95c688
Arun Balajee Vasudevan <arunbalajeev@iitj.ac.in>
2015-05-08 12:23:58 +0200

    ExtractPatches2 has been modified

--- preprocessing/src/main/java/ParseJSON.java
+++ preprocessing/src/main/java/ParseJSON.java
@@ -196,63 +196,127 @@ processLines takes as input the image of the page which is of type Mat and the l
 		}
 		return results;
 	}
-	public static List<Vector> extractPatches2(Integer[][] boundary) {
-		List<Vector> results = new ArrayList<>();
-		int stepSize;
+	public static List<Tuple2<Vector,Vector>> extractPatches2(Tuple2<Integer[],Integer[][]>  boundary) {
+		List<Tuple2<Vector,Vector>> results = new ArrayList<>();
+		int patchSize;
+		int stepSizex;
+		int stepSizey;
+		stepSizex = sizex/2;
+		stepSizey = sizey;
 		if(sizex > sizey)
-			stepSize = sizey/2;
+			patchSize = sizey/2;
 		else
-			stepSize = sizex/2;
+			patchSize = sizex/2;
 		
-		sortArray(boundary,1);
-		int xmin = boundary[0][1];
+		//sortArray(boundary,1);
+		//int xmin = boundary[0][1];
 		//System.out.println("xmin: "+ xmin);
-		int xmax = boundary[boundary.length-1][1];
-		Integer[]tempboundary = new Integer[boundary.length];
-		Integer[]tempboundary2= new Integer[boundary.length];
-		for(int s = 0; s < boundary.length; s++)
-		{
-			tempboundary[s] = boundary[s][1];
-		}
+		//int xmax = boundary[boundary.length-1][1];
+		sortArray(boundary._2,1);
+		int xmin = boundary._2[0][1];
+		int xmax = boundary._2[boundary._2.length-1][1];
+		//sortArray(boundary._2,0);
+		//int ymin = boundary._2[0][0];
+		//int ymax = boundary._2[boundary._2.length-1][0];
 		
-		for(int i = xmin + stepSize; i <= xmax-stepSize ; i = i+stepSize)
+		Integer[]tempboundary = new Integer[boundary._2.length];
+		Integer[]tempboundary2= new Integer[boundary._2.length];
+		for(int s = 0; s < boundary._2.length; s++)
+		{
+			tempboundary[s] = boundary._2[s][1];
+		}
+		int stepSize = patchSize;
+		for(int i = xmin; i <= xmax+stepSizex/2 ; i = i+stepSizex)
 		{
 			//System.out.println("x: "+i);
 			ArrayList<Integer> yval = new ArrayList<Integer>();
 			int ind = 0;
-			int val = Arrays.binarySearch(tempboundary, i);
+			int val =-1;
+			int ival = i;
+			for(int l=0;l<tempboundary.length;l++){
+				if(tempboundary[l]==ival){
+					val=l;
+					break;
+				}
+			}
 		    tempboundary2 = Arrays.copyOfRange(tempboundary, 0, tempboundary.length);
-			while(val >= 0)
+		    int l=0;
+		    int range = 2*stepSizex;
+		    if(i==xmin+stepSizex/2){range = 2*stepSizex; ival = i-stepSizex/2;}
+		    if(i>xmin+stepSizex/2){range = stepSizex; ival = i-stepSizex;}
+		    //System.out.println("val:"+val);
+			while(ival<i+range)
 			{
-				yval.add(boundary[val+ind][0]);
+				if(val!=-1)
+				{yval.add(boundary._2[val+ind][0]);}
 				tempboundary2 = Arrays.copyOfRange(tempboundary2, val+1, tempboundary2.length);
 				//System.out.println("uzunluk" + tempboundary2.length);
 				ind += val+1;
-				val = Arrays.binarySearch(tempboundary2, i);
+				val=-1;
+				for(l=0;l<tempboundary2.length;l++){
+					if(tempboundary2[l]==ival){
+						val=l;
+						break;
+					}
+				}
+				if(val==-1)
+					{ival++;}
 			}
-			if(yval.size() != 0)
+			//System.out.println("yval.size:"+yval.size());
+			if(yval.size() >= 2)
 			{
 				int ymin = Collections.min(yval);
 				int ymax = Collections.max(yval);
 				//System.out.println("ymin: "+ ymin + " ymax: " + ymax);
-				for(int j = ymin +stepSize; j <= ymax-stepSize ; j= j+stepSize)
+				for(int j = ymin; j <= ymax; j= j+stepSizey)
 				{
 					double[] patchData = new double[sizex*sizey];
 					byte[] lineData = new byte[numCols];
 					Mat pat = new Mat();
-					if((i-stepSize+1 >= 0) && (i+stepSize+1 <= numRows) && (j-stepSize+1 >= 0) && (j+stepSize+1 <= numCols))
+					if((i-patchSize+1 >= 0) && (i+patchSize+1 <= numRows) && (j-(2*patchSize)+1 >= 0) && (j+(2*patchSize)+1 <= numCols))
 					{
 						if(stepSize % 2 == 0)
 						{
-							pat = img.submat(i-stepSize+1, i+stepSize+1, j-stepSize+1, j+stepSize+1);
+							pat = img.submat(i-patchSize+1, i+patchSize+1, j-patchSize+1, j+patchSize+1);
 							//System.out.println(pat.size().height);
 						}
 						else
 						{
-							pat = img.submat(i-stepSize, i+stepSize+1, j-stepSize, j+stepSize+1);
+							pat = img.submat(i-patchSize, i+patchSize+1, j-patchSize, j+patchSize+1);
 						}
-						Highgui.imwrite("/home/isinsu/Desktop/DeepManuscripts/preprocessing/pat" +saveindex+".tif", pat);
-						saveindex = saveindex + 1;
+						double total = 0;
+						for(int a = 0; a < pat.rows(); a++)
+						{
+							for (int b = 0; b < pat.cols(); b++)
+							{
+								byte [] pat_data = new byte[1];
+								pat.get(a, b, pat_data);
+								double pat_data_val = (pat_data[0]>= 0 ? pat_data[0] : 256+pat_data[0]);
+								total += pat_data_val;
+								
+							}
+						}
+						double mean = total/(pat.cols()*pat.rows());
+						
+						double var = 0;
+						
+						for(int a = 0; a < pat.rows(); a++)
+						{
+							for (int b = 0; b < pat.cols(); b++)
+							{
+								byte [] pat_data = new byte[1];
+								pat.get(a, b, pat_data);
+								double pat_data_val = (pat_data[0]>= 0 ? pat_data[0] : 256+pat_data[0]);
+								var += ((pat_data_val - mean) * (pat_data_val - mean));
+								
+							}
+						}
+						var /= (pat.rows()*pat.cols());
+						double std_dev = Math.sqrt(var);
+						if(std_dev > 15)
+						{
+						//Highgui.imwrite("/home/arun/Big Data Project/DeepManuscript/preprocessing/pat/" +saveindex+".tif", pat);
+						//saveindex = saveindex + 1;
 						Mat temppatch = pat.clone();
 						Mat patch = temppatch.t();
 						patch = patch.reshape(0, 1);
@@ -263,9 +327,16 @@ processLines takes as input the image of the page which is of type Mat and the l
 							patch.get(0, k, data);
 							patchData[k] = (data[0]>= 0 ? data[0] : 256+data[0]);
 						}
+						double [] coord = new double[6];
+						coord[0] = boundary._1[0];
+						coord[1] = boundary._1[1];
+						coord[2] = i-(sizex/2);
+						coord[3] = j-(sizey/2);
+						coord[4] = sizex;
+						coord[5] = sizey;
 		
-						results.add(new DenseVector(patchData)); 
-						
+						results.add(new Tuple2<Vector, Vector>(new DenseVector(coord),new DenseVector(patchData))); 
+						}
 					}
 				}
 			}
